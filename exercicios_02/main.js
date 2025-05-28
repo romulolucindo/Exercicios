0c692592c8b167f2de9a0d0d2ded98c30404be86
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-05-28 00:41:55 -0300

    Exercicios 2 completos

--- exercicios_02/main.js
+++ exercicios_02/main.js
@@ -1,6 +1,6 @@
 // Desafios
 // Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
-let dia = prompt('Hoje é qual dia da samana?');
+let dia = prompt('Hoje é qual dia da semana?');
 
 if (dia == 'Sabado' || dia == 'Domingo') {
   alert('Bom final de semana!!');
@@ -14,9 +14,15 @@ if (numero_info >= 0) {
 } else alert('Por favor, digite um numero P O S I T I V O !!!');
 
 // Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
-
+let pontos = 101;
+if (pontos >= 100) {
+  alert('Parabens vc venceu!');
+} else alert('Tente novamente para ganhar!');
 
 // Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+let saldo = 1000;
+alert(`O saldo da sua conta é: ${saldo}`);
 
 // Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
-
+let perg_nome = prompt('Qual o seu nome?');
+alert(`Seja bem-vindo ${perg_nome}`);
