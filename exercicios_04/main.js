69b60151a84f079813dba1f5bc8b1b805bc215c1
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-06-18 12:10:22 -0300

    Exercicios_04 Completos

--- exercicios_04/main.js
+++ exercicios_04/main.js
@@ -30,16 +30,45 @@ console.log(`A diferença de ${valor_1} e ${valor_2} é igual a ${resultado_dife
 
 // Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
 
-
+let perg_idade = prompt('Qual a sua idade?');
+if (perg_idade >= 18) {
+  alert('Acesso permitido, Usuario maior de Idade!');
+} else {
+  alert('Acesso NEGADO, Usuario menor de Idade!');
+}
 
 // Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+let perg_numero = Number(prompt('Digite um qualquer numero'));
+if (perg_numero > 0) {
+  alert('Numero positivo');
+} else if (perg_numero < 0) {
+  alert('Numero negativo')
+} else {
+  alert('Numero 0');
+}
 
-// Use um loop while para imprimir os números de 1 a 10 no console.
+//Use um loop while para imprimir os números de 1 a 10 no console.
+let contar = 1;
+while (contar <= 10) {
+  console.log(contar);
+  contar++;
+}
 
-// Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+//Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+let nota = 10;
 
-// Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+if (nota >= 7) {
+  alert('Aprovado :D')
+} else {
+  alert('Reprovado T-T')
+}
 
-// Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+//Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+console.log(Math.random());
 
-// Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+
+//Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+console.log(parseInt(Math.random() * 10 + 1));
+
+//Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+console.log(parseInt(Math.random() * 1000 + 1));
