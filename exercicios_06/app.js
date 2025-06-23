07907d671de3fa7c4abdd0456e195a6a9af1ca13
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-06-23 11:38:20 -0300

    Exercicio_06 não concluido

---
+++ exercicios_06/app.js
@@ -0,0 +1,28 @@
+// Criar uma função que exibe "Olá, mundo!" no console.
+function helloWorld() {
+  let saudacao = 'Hello World!';
+  console.log(saudacao);
+}
+helloWorld();
+
+// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+function pergunta(nome) {
+  console.log(`Olá ${nome}!`);
+}
+pergunta('José');
+
+// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function double(numero) {
+  return numero * 2;
+}
+console.log(double(20));
+
+// Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function media(num1, num2, num3) {
+  return ((num1 + num2 + num3) / 3);
+}
+console.log(media(10, 20, 30));
+
+// Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+// Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
