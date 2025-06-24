aeb307dce9ff759d49001139fb58791ade954a61
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-06-24 09:29:25 -0300

    Exercicio_06 Completo

--- exercicios_06/app.js
+++ exercicios_06/app.js
@@ -24,5 +24,13 @@ function media(num1, num2, num3) {
 console.log(media(10, 20, 30));
 
 // Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+function maior(num1, num2) {
+  return (Math.max(num1, num2));
+}
+console.log(maior(80, 50));
 
 // Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+function multi(num1) {
+  return num1 * num1;
+}
+console.log(multi(11));
