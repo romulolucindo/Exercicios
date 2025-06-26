c0a09a0600717a1fc4f555cb60d869fed74603b6
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-06-26 18:04:08 -0300

    Exercicio_07 concluido

--- exercicios_06/app.js
+++ exercicios_06/app.js
@@ -1,36 +1,58 @@
-// Criar uma função que exibe "Olá, mundo!" no console.
-function helloWorld() {
-  let saudacao = 'Hello World!';
-  console.log(saudacao);
+// Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+function imc(peso, altura) {
+  let calculoImc = peso / (altura * altura);
+  return calculoImc.toFixed(1);
 }
-helloWorld();
+console.log(imc(78, 1.90));
 
-// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
-function pergunta(nome) {
-  console.log(`Olá ${nome}!`);
-}
-pergunta('José');
+// Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
 
-// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
-function double(numero) {
-  return numero * 2;
+function fatorial(numero) {
+  let resultado = 1;
+  while (numero > 1) {
+    resultado = resultado * numero;
+    numero--;
+  }
+  return resultado;
 }
-console.log(double(20));
+console.log(fatorial(10));
 
-// Criar uma função que recebe três números como parâmetros e retorna a média deles.
-function media(num1, num2, num3) {
-  return ((num1 + num2 + num3) / 3);
+// Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+function convesor_moeda(valor) {
+  let cotacao = Number(4.80);
+  return (valor / cotacao);
 }
-console.log(media(10, 20, 30));
+console.log(`R$ ${convesor_moeda(1000).toFixed(2)}`);
 
-// Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
-function maior(num1, num2) {
-  return (Math.max(num1, num2));
-}
-console.log(maior(80, 50));
+// Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+function calcularSala(altura, largura) {
+  let area = largura * altura;
+  let perimetro = 2 * (largura + altura)
 
-// Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
-function multi(num1) {
-  return num1 * num1;
+  console.log(`A area da sala é ${area} m²`);
+  console.log(`O perimetro da sala é ${perimetro} m`);
 }
-console.log(multi(11));
+calcularSala(10, 20);
+
+// Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+function calcularSalaCircular(raio) {
+  let pi = 3.14;
+
+  let area = pi * (raio * raio).toFixed(2);
+  let perimetro = 2 * (pi * raio).toFixed(2);
+
+  console.log(`A area da sala circular é ${area} m²`);
+  console.log(`O perimetro da sala circular é ${perimetro} m`);
+}
+calcularSalaCircular(10);
+
+// Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+function tabuada(num) {
+  let contador = 1;
+  while (contador <= 10) {
+    let result_tabuada = num * contador;
+    console.log(result_tabuada);
+    contador++;
+  }
+}
+tabuada(parseInt(prompt('Qual a taboada que vc quer ver?')));
