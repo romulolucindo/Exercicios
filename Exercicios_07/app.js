c0301c26d874722a5336b689f05aa3feadc9b2d8
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-06-27 13:45:24 -0300

    Exercicio_07 concluido

---
+++ Exercicios_07/app.js
@@ -0,0 +1,18 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let lista_generica = [];
+
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let lista_linguagem = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+console.log(lista_linguagem);
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+lista_linguagem.push('Java', 'Ruby', 'Golang');
+console.log(lista_linguagem);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+console.log(lista_linguagem[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(lista_linguagem[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(lista_linguagem[lista_linguagem.length - 1]);
