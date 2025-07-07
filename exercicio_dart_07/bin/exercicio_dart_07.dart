323c557d8b27ddb49d1415fedcfbd512c75beff7
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-07-07 14:06:45 -0300

    Exercicio_dart_07 Completo

--- exercicio_dart_07/bin/exercicio_dart_07.dart
+++ exercicio_dart_07/bin/exercicio_dart_07.dart
@@ -1,8 +1,16 @@
+import 'dart:io';
 import 'package:http/http.dart';
 import 'dart:convert';
 
 void main() async {
-  await getLivros("jorge Amado");
+  await getLivros("Jorge Amado");
+  print("Digite quais ingredientes vc tem disponivel pf?");
+  String input = stdin.readLineSync()!;
+  List<String> ingredientesDisponiveis = input
+      .split(',')
+      .map((e) => e.trim())
+      .toList();
+  await getRecipe(ingredientesDisponiveis);
 }
 
 // Exercício 1: Buscando livros por autor em uma biblioteca digital
@@ -25,7 +33,40 @@ Future<void> getLivros(String authotDesejado) async {
         encontrou = true;
       }
     }
+    if (!encontrou) {
+      print("Nenhum livro encontrado.");
+    }
   } else {
     print("Erro ao buscar livros: ${response.statusCode}");
+  }
+}
+
+// Exercício 2: Filtrando receitas por ingredientes disponíveis
+// Você trabalha em um aplicativo de receitas culinárias que ajuda os usuários a encontrar receitas baseadas nos ingredientes que têm em casa.
+// As receitas são armazenadas em formato JSON e incluem uma lista de ingredientes.
+// Sua tarefa é criar uma função em Dart que filtre as receitas que podem ser feitas com os ingredientes disponíveis que deverão ser fornecidas por uma lista de String no parâmetro na função, e exiba os nomes das receitas que podem ser preparadas com esses ingredientes.
+Future<void> getRecipe(List<String> ingredientesDisponiveis) async {
+  String url =
+      "https://gist.githubusercontent.com/romulolucindo/88e9c81baec91475063462d53997804e/raw/33cb2b2a5efe46f64753f6f90ac9b7d3403afa2a/recipes.json";
+  Response response = await get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    List<dynamic> receitas = json.decode(response.body);
+    print('Receitas que você pode fazer:');
+    bool encontrou = false;
+
+    for (var receita in receitas) {
+      if (receita["ingredients"].every(
+        (i) => ingredientesDisponiveis.contains(i),
+      )) {
+        print('- ${receita['nome']}');
+        encontrou = true;
+      }
+    }
+    if (!encontrou) {
+      print("Nenhuma receita encontrada.");
+    }
+  } else {
+    print("Erro ao buscar receitas: ${response.statusCode}");
   }
 }
