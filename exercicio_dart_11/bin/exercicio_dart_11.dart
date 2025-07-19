c164e3942ce4c41a0286acb449ae60409de2ee91
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-07-19 12:09:12 -0300

    Exercicio_dart_10 Incompleta

---
+++ exercicio_dart_11/bin/exercicio_dart_11.dart
@@ -0,0 +1,46 @@
+// Em um sistema de vendas, a loja precisa calcular o valor total dos produtos. Crie uma função que receba um mapa de produtos e calcule o valor total dos produtos.
+void main() {
+  exibirValorTotal();
+  exibirProdutos2();
+}
+
+void exibirValorTotal() {
+  Map<String, double> precosProdutos = {
+    "Camiseta": 30.0,
+    "Calça": 50.0,
+    "Boné": 15.0,
+    "Tênis": 120.0,
+  };
+  double totalProduto = 0;
+
+  for (double precoProduto in precosProdutos.values) {
+    totalProduto += precoProduto;
+  }
+  print('O total da compra é: $totalProduto');
+}
+
+// Você tem um mapa com o nome dos produtos e a quantidade em estoque de cada um. A loja quer saber quantos itens no total ainda tem no estoque (somando as quantidades de todos os produtos).
+void exibirProdutos2() {
+  Map<String, int> estoque = {
+    "Camiseta": 10,
+    "Calça": 5,
+    "Boné": 12,
+    "Tênis": 4,
+  };
+  int estoqueTotal = 0;
+  for (int itens in estoque.values) {
+    estoqueTotal += itens;
+  }
+  print('O estoque total é: $estoqueTotal itens');
+}
+
+// Em um sistema de inventário, é necessário realizar ajustes de preço de determinados produtos.
+//Crie uma função que receba um mapa de produtos, um nome de produto e um novo preço, atualizando o valor e exibindo o inventário atualizado.
+
+void exibirInventario() {
+  Map<String, double> inventarioProdutos = {
+    "Camiseta": 30.0,
+    "Calça": 50.0,
+    "Boné": 15.0,
+  };
+}
