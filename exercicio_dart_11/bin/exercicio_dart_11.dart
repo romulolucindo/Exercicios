b608524fbf7dcc83cbfc6cf112ef3861a14539a8
Romulo Lucindo <romulolucindo29@hotmail.com>
2025-07-21 15:39:05 -0300

    exercicio_dart_11 Completo

--- exercicio_dart_11/bin/exercicio_dart_11.dart
+++ exercicio_dart_11/bin/exercicio_dart_11.dart
@@ -1,7 +1,16 @@
 // Em um sistema de vendas, a loja precisa calcular o valor total dos produtos. Crie uma função que receba um mapa de produtos e calcule o valor total dos produtos.
+import 'dart:io';
+
+Map<String, double> inventarioProdutos = {
+  "Camiseta": 30.0,
+  "Calça": 50.0,
+  "Boné": 15.0,
+};
+
 void main() {
   exibirValorTotal();
   exibirProdutos2();
+  exibirInventario();
 }
 
 void exibirValorTotal() {
@@ -38,9 +47,36 @@ void exibirProdutos2() {
 //Crie uma função que receba um mapa de produtos, um nome de produto e um novo preço, atualizando o valor e exibindo o inventário atualizado.
 
 void exibirInventario() {
-  Map<String, double> inventarioProdutos = {
-    "Camiseta": 30.0,
-    "Calça": 50.0,
-    "Boné": 15.0,
-  };
+  double novoPreco = 0;
+
+  print('Bem-vindo ao estoque de nossa rede, esse é o nosso estoque atual:');
+  print(inventarioProdutos);
+
+  print('Digite qual o produto que vc quer alterar o preço:');
+  String entradaProduto = stdin.readLineSync()!;
+
+  if (!inventarioProdutos.containsKey(entradaProduto)) {
+    print('Produto não cadastrado em nosso estoque!');
+    exibirInventario();
+    return;
+  }
+  print('Digite o novo valor:');
+  novoPreco = double.parse(stdin.readLineSync()!);
+
+  atualizarInventario(inventarioProdutos, entradaProduto, novoPreco);
+}
+
+void atualizarInventario(
+  Map<String, double> inventario,
+  String nomeProduto,
+  double novoPreco,
+) {
+  if (!inventario.containsKey(nomeProduto)) {
+    print('Produto não cadastrado em nosso estoque!');
+    return;
+  }
+
+  inventario.update(nomeProduto, (value) => novoPreco);
+  print('Inventário atualizado:');
+  print(inventario);
 }
