556dab06a0ab77f7290fa2aafe0efac86e12cfc9
romulolucindo <romulolucindo29@gmail.com>
2026-03-12 17:51:19 -0300

    Exercicio_Python_06 Incompleto

---
+++ Exercicio_Python/Exercicio_Python_06.py
@@ -0,0 +1,71 @@
+# 1) Escreva um programa que peça dois números inteiros e imprima todos os números inteiros entre eles.
+valor1 = int(input('Escreva um numero '))
+valor2 = int(input('Escreva outro numero '))
+
+if valor1 > valor2:
+    for contador in range(valor2 +1, valor1):
+        print(contador)
+
+else:
+    for contador in range(valor1 +1, valor2):
+        print(contador)
+    
+
+# 2) Escreva um programa para calcular quantos dias levará para a colônia de uma bactéria A ultrapassar ou igualar a colônia de uma bactéria B, com base nas taxas de crescimento de 3% e 1,5% respectivamente. Considere que a colônia A inicia com 4 elementos e a B com 10.
+
+contador = 0
+taxa1 = 0.03
+taxa2 = 0.015
+colonia1 = 4
+colonia2 = 15
+
+while colonia1 < colonia2:
+    colonia1 = colonia1 * (1 + taxa1)
+    colonia2 = colonia2 * (1 + taxa2)
+    contador += 1
+
+print('Dias nescessarios: ',contador)
+
+# 3) Para tratar uma quantidade de 15 dados de avaliações de pessoas usuárias de um serviço da empresa, precisamos verificar se as notas são válidas. Então, escreva um programa que vai receber a nota de 0 a 5 de todos os dados e verificar se é um valor válido. Caso seja inserido uma nota acima de 5 ou abaixo de 0, repita até que a pessoa usuária insira um valor válido.
+
+contador_avaliação = 0
+
+while contador_avaliação < 15:
+    nota = int(input('Dê uma nota de avaliação de 0 a 5: '))
+    if nota > 5:
+        print('Valor Inválido, tente novamente!')
+        continue
+    elif nota < 0:
+        print('Valor Inválido, tente novamente!')
+        continue
+    else:
+        print('Obrigado pela sua avaliação!')
+        contador_avaliação +=1
+
+
+
+
+
+
+# 4) Desenvolva um programa que leia um conjunto indeterminado de temperaturas em Celsius e informe a média delas. A leitura deve ser encerrada ao ser enviado o valor -273°C.
+
+# 5) Escreva um programa que calcule o fatorial de um número inteiro fornecido pela pessoa usuária. Lembrando que o fatorial de um número inteiro é a multiplicação desse número por todos os seus antecessores até o número 1. Por exemplo, o fatorial de 5 é 5 x 4 x 3 x 2 x 1 = 120.
+
+# Momento dos projetos
+# 6) Escreva um programa que gere a tabuada de um número inteiro de 1 a 10, de acordo com a escolha da pessoa usuária. Como exemplo, para o número 2, a tabuada deve ser mostrada no seguinte formato:
+
+# Tabuada do 2:
+# 2 x 1 = 2
+# 2 x 2 = 4
+# [...]
+# 2 x 10 = 20
+# Copiar código
+# 7) Os números primos possuem várias aplicações dentro da Ciência de Dados em criptografia e segurança, por exemplo. Um número primo é aquele que é divisível apenas por um e por ele mesmo. Assim, faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
+
+# 8) Vamos entender a distribuição de idades de pensionistas de uma empresa de previdência. Escreva um programa que leia as idades de uma quantidade não informada de clientes e mostre a distribuição em intervalos de [0-25], [26-50], [51-75] e [76-100]. Encerre a entrada de dados com um número negativo.
+
+# 9) Em uma eleição para gerência em uma empresa com 20 pessoas colaboradoras, existem quatro candidatos(as). Escreva um programa que calcule o(a) vencedor(a) da eleição. A votação ocorreu da seguinte maneira:
+
+# Cada colaborador(a) votou em uma das quatro pessoas candidatas (que representamos pelos números 1, 2, 3 e 4).
+# Também foram contabilizados os votos nulos (representados pelo número 5) e os votos em branco (representados pelo número 6).
+# Ao final da votação, o programa deve exibir o número total de votos para cada candidato(a), os nulos e os votos em branco. Além disso, deve calcular e exibir a porcentagem de votos nulos em relação ao total de votos e a porcentagem de votos em branco em relação ao total de votos.
---
+++ Rascunho.py
@@ -0,0 +1,4 @@
+numero = 0
+while numero <= 10:
+    print(numero)
+    numero = numero+1
